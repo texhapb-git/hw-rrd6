7d5cf6acb50fd36f50a47ff8a8898c542a48113b
TexHaPb <texhapb@bk.ru>
2023-01-04 14:27:08 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+// import { Routes, Route, Navigate, Outlet, useRoutes } from "react-router-dom";
+import { Navigate, Outlet, useRoutes } from "react-router-dom";
+import EditUser from "./components/EditUser";
+import UserProfile from "./components/UserProfile";
+import UsersList from "./components/UsersList";
 import HomePage from "./pages/HomePage";
 import UsersPage from "./pages/UsersPage";
 
-
 function App() {
+  const routes = useRoutes([
+    {
+      path: "",
+      element: <HomePage />
+    },
+    {
+      path: "users",
+      element: <UsersPage />,
+      children: [
+        {
+          index: true,
+          element: <UsersList />
+        },
+        {
+          path: ":userId",
+          element: <Outlet />,
+          children: [
+            {
+              index: true,
+              element: <Navigate to="profile" />
+            },
+            {
+              path: "profile",
+              element: <UserProfile />
+            },
+            {
+              path: "edit",
+              element: <EditUser />
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: "*",
+      element: <Navigate to="/" />
+    }
+  ]);
+
   return (
-    <BrowserRouter>
-      <div className="app">
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/users" component={UsersPage} />
-          <Redirect to="/" />
-        </Switch>
-      </div>
-    </BrowserRouter >
+    <div className="app">
+      {routes}
+      {/* <Routes>
+        <Route path="" element={<HomePage />} />
+        <Route path="users" element={<UsersPage />}>
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<Outlet />}>
+            <Route index element={<Navigate to="profile" />} />
+            <Route path="profile" element={<UserProfile />} />
+            <Route path="edit" element={<EditUser />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes > */}
+    </div>
 
   );
 }
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,16 +1,8 @@
-import { Redirect, Route, Switch } from "react-router-dom";
-import EditUser from "../components/EditUser";
-import UserProfile from "../components/UserProfile";
-import UsersList from "../components/UsersList";
+import { Outlet } from "react-router-dom";
 
 const UsersPage = () => {
 	return (
-		<Switch>
-			<Route path="/users" exact component={UsersList} />
-			<Route path="/users/:userId/profile" component={UserProfile} />
-			<Route path="/users/:userId/edit" component={EditUser} />
-			<Redirect from="/users/:userId" to="/users/:userId/profile" />
-		</Switch>
+		<Outlet />
 	)
 }
 
